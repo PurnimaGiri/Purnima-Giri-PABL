c6ca6d673369aed2a87337a0db40a80806f5b4a6
PurnimaGiri <purnimagiri156@gmail.com>
2026-02-25 19:54:09 +0530

    Add files via upload
    added 3rd_lect tasks from task 16 to 21

---
+++ 2nd_lect/task_21.py
@@ -0,0 +1,11 @@
+# Given an array of non negative numbers each 
+# number tells you the maximum number of steps you can jump
+# for ward from that position, find minimum number of jumps needed
+# to move from first to last position
+
+# for i in range(0,len(arr)-1):
+#     i = i + arr[i]
+#     for j in range(i,len(arr)):
+#         jump = jump+1
+
+    
